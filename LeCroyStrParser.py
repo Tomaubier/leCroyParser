8d32502e1fc4b7d718effd0f0bb3c66b3bb7bec4
Tom Aubier <tom.aubierlaure@orange.fr>
2019-07-12 11:00:43 +0200

    Support for binary string decoding

--- LeCroyStrParser.py
+++ LeCroyStrParser.py
@@ -1,7 +1,7 @@
-""" leCroyParser.py
+""" LecroyStrParser.py
 (c) Benno Meier, 2018 published under an MIT license.
 
-leCroyParser.py is derived from the matlab programme ReadLeCroyBinaryWaveform.m,
+LecroyStrParser.py is derived from the matlab programme ReadLeCroyBinaryWaveform.m,
 which is available at Matlab Central.
 a useful resource for modifications is the LeCroy Remote Control Manual
 available at http://cdn.teledynelecroy.com/files/manuals/dda-rcm-e10.pdf
@@ -12,53 +12,25 @@ Fachstelle Elektronik
 Slightly modified by Alan Blankman, LeCroy Corporation, 2006
 
 Further elements for the code were taken from pylecroy, written by Steve Bian
+Slightly modified by Tom Aubier to support binary string decoding.
 
-lecroyparser defines the ScopeData object.
-Tested in Python 2.7 and Python 3.6
+LecroyStrParser defines the ScopeData object.
+Tested in Python 3.7
 """
 
 import sys
 import numpy as np
-import glob
 
+class ScopeData:
+    """
+    Decodes LeCroy scope data stored in binaryString given as an argument.
+    """
+    def __init__(self, binaryString):
+        self.binaryString = binaryString
+        self.x, self.y = self.parseBinaryData()
 
-class ScopeData(object):
-    def __init__(self, path, parseAll = False, sparse = -1):
-        """Import Scopedata as stored under path.
-
-        If parseAll is set to true, search for all files 
-        in the folder commencing with C1...Cx and store the y data in a list.
-
-        If a positive value is provided for sparse, then only #sparse elements will 
-        be stored in x and y. These will be sampled evenly from all data points in 
-        the source file. This can speed up data processing and plotting."""
-
-        
-        self.path = path
-
-        if parseAll:
-            basePath = "/".join(path.split("/")[:-1])
-            core_filename = path.split("/")[-1][2:]
-
-            files = sorted(list( glob.iglob(basePath + "/C*" + core_filename)))
-
-            self.y = []
-            for f in files:
-                x, y = self.parseFile(f, sparse = sparse)
-                self.x = x
-                self.y.append(y)
-                
-        else:
-            x, y = self.parseFile(path, sparse = sparse)
-            self.x = x
-            self.y = y
-
-        
-    def parseFile(self, path, sparse = -1):
-        self.file = open(path, mode='rb')
+    def parseBinaryData(self):
         self.endianness = "<"
-        
-        fileContent = self.file.read()
 
         waveSourceList = ["Channel 1", "Channel 2", "Channel 3", "Channel 4", "Unknown"]
         verticalCouplingList = ["DC50", "GND", "DC1M", "GND", "AC1M"]
@@ -68,13 +40,13 @@ class ScopeData(object):
                           "centered_RIS", "peak_detect"]
         processingList = ["No Processing", "FIR Filter", "interpolated", "sparsed",
                           "autoscaled", "no_resulst", "rolling", "cumulative"]
-        
+
         #convert the first 50 bytes to a string to find position of substring WAVEDESC
-        self.posWAVEDESC = fileContent[:50].decode("ascii").index("WAVEDESC")
-        
+        self.posWAVEDESC = self.binaryString[:50].decode("ascii").index("WAVEDESC")
+
         self.commOrder = self.parseInt16(34) #big endian (>) if 0, else little
         self.endianness = [">", "<"][self.commOrder]
-        
+
         self.templateName = self.parseString(16)
         self.commType = self.parseInt16(32) # encodes whether data is stored as 8 or 16bit
 
@@ -94,7 +66,7 @@ class ScopeData(object):
         self.verticalOffset = self.parseFloat(160)
 
         self.nominalBits = self.parseInt16(172)
-        
+
         self.horizInterval = self.parseFloat(176)
         self.horizOffset = self.parseDouble(180)
 
@@ -108,49 +80,36 @@ class ScopeData(object):
         self.verticalCoupling = verticalCouplingList[self.parseInt16(326)]
         self.bandwidthLimit = bandwidthLimitList[self.parseInt16(334)]
         self.waveSource = waveSourceList[self.parseInt16(344)]
-        
 
-        self.file.seek(self.posWAVEDESC + self.waveDescriptor + self.userText
-                       + self.trigTimeArray)
+        self.startIndex = self.posWAVEDESC + self.waveDescriptor + self.userText + self.trigTimeArray
+        # self.file.seek()
 
         if self.commType == 0: #data is stored in 8bit integers
-            y = np.fromstring(self.file.read(self.waveArray1), dtype = np.dtype((self.endianness + "i1", self.waveArray1)))[0]
+            y = np.frombuffer(self.binaryString[self.startIndex:self.startIndex+self.waveArray1], dtype = np.dtype((self.endianness + "i1", self.waveArray1)))[0]
         else: #16 bit integers
             length = self.waveArray1//2
-            y = np.fromstring(self.file.read(self.waveArray1), dtype = np.dtype((self.endianness + "i2", length)))[0]
+            y = np.frombuffer(self.binaryString[self.startIndex:self.startIndex+self.waveArray1], dtype = np.dtype((self.endianness + "i2", length)))[0]
 
         #now scale the ADC values
         y = self.verticalGain*np.array(y) - self.verticalOffset
-        
+
         x = np.linspace(0, self.waveArrayCount*self.horizInterval,
                              num = self.waveArrayCount) + self.horizOffset
-        
-        if sparse > 0:
-            indices = int(len(x) / sparse) * np.arange(sparse)
 
-            x = x[indices]
-            y = y[indices]
-
-            self.file.close()
         return x, y
 
-        
     def unpack(self, pos, formatSpecifier, length):
         """ a wrapper that reads binary data
-        in a given position in the file, with correct endianness, and returns the parsed
+        in a given position in the binary data string, with correct endianness, and returns the parsed
         data as a tuple, according to the format specifier. """
-        self.file.seek(pos + self.posWAVEDESC)
-        x = np.fromstring(self.file.read(length), self.endianness + formatSpecifier)[0]
+        x = np.frombuffer(self.binaryString[pos+self.posWAVEDESC:pos+self.posWAVEDESC+length], self.endianness + formatSpecifier)[0]
         return x
-    
-        
+
     def parseString(self, pos, length = 16):
         s = self.unpack(pos, "S{}".format(length), length)
         if sys.version_info > (3, 0):
             s = s.decode('ascii')
         return s
-
-    #return "".join(map(chr, self.unpack(pos, 's'*16, 16)))
 
     def parseInt16(self, pos):
         return self.unpack(pos, "u2", 2)
@@ -177,7 +136,7 @@ class ScopeData(object):
         day = self.parseByte(pos + 10)
         month = self.parseByte(pos + 11)
         year = self.parseWord(pos + 12)
-        
+
         return "{}-{:02d}-{:02d} {:02d}:{:02d}:{:.2f}".format(year, month, day,
                                                               hour, minute, second)
 
@@ -195,10 +154,9 @@ class ScopeData(object):
             return "{} ".format(value) + unit.strip() + "s/div"
         elif timeBaseNumber == 100:
             return "EXTERNAL"
-           
+
     def __repr__(self):
         string = "Le Croy Scope Data\n"
-        string += "Path: " + self.path + "\n"
         string += "Endianness: " + self.endianness + "\n"
         string += "Instrument: " + self.instrumentName + "\n"
         string += "Instrunemt Number: " + str(self.instrumentNumber) + "\n"
@@ -210,13 +168,5 @@ class ScopeData(object):
         string += "Processing: " + self.processingDone + "\n"
         string += "TimeBase: " + self.timeBase + "\n"
         string += "TriggerTime: " + self.triggerTime + "\n"
-        
+
         return string
-
-
-
-
-
-if __name__ == "__main__":
-    data = ScopeData(path, parseAll = True)
-
---
+++ README.md
@@ -0,0 +1,19 @@
+# LecroyStrParser
+
+LeCroyStrParser.py parses binary str produced by LeCroy scopes.
+
+LeCroyStrParser.py is derived from the matlab program
+ReadLeCroyBinaryWaveform.m, available at
+<https://www.mathworks.com/matlabcentral/fileexchange/26375-readlecroybinarywaveform-m>,
+Original version (c)2001 Hochschule fr Technik+Architektur Luzern
+Fachstelle Elektronik 6048 Horw, Switzerland Slightly modified by Alan
+Blankman, LeCroy Corporation, 2006
+
+Further elements for the code were taken from pylecroy, written by Steve Bian
+Slightly modified by Tom Aubier to support binary string decoding.
+
+A useful resource for modifications is the LeCroy Remote Control Manual
+available at
+<http://cdn.teledynelecroy.com/files/manuals/dda-rcm-e10.pdf>
+
+LecroyStrParser has been tested with and Python 3.7
